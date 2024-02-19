9efe9a1d0e795ec82f0ca69b515e786f389ce0c2
Dashka22 <dashka.nest1@gmail.com>
2024-02-19 18:11:48 +0800

    skeleton

--- portfolio/src/pages/index.jsx
+++ portfolio/src/pages/index.jsx
@@ -1,33 +1,16 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  console.log("HAHAHHAHAHAH");
+  const fetchData = async () => {
+    const res = await fetch("https://randomuser.me/api/?results=10");
+    setUsers(await res.json());
+  };
+
   useEffect(() => {
-    async function fetchData() {
-      console.log();
-      const res = await fetch("https://randomuser.me/api/?results={count}");
-      setData(await res.json());
-    }
     fetchData();
-  }, [count]);
+  }, []);
 
-  console.log("Hi");
-  console.log("Hi");
-  return (
-    <div>
-      <div>
-        <button
-          className=" border-[2px] rounded-md border-red-400 w-[200px] h-[50px]"
-          onClick={() => setCount(count + 1)}
-        >
-          Add
-        </button>
-      </div>
-
-      <h1>{count}</h1>
-    </div>
-  );
+  return <div>{users && users?.map((user) => <>{user?.name?.first}</>)}</div>;
 }
--- portfolio/src/pages/promise.jsx
+++ portfolio/src/pages/promise.jsx
@@ -1,91 +1,60 @@
 import { useEffect, useState } from "react";
 
 export default function Promise() {
-  const [articles, setArticles] = useState([]);
-  const [filteredArray, setFilteredArray] = useState(articles);
-  const [topArticlesData, setTopArticlesData] = useState(articles);
-  const [risinArticlesData, setRisinArticlesData] = useState(articles);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = async () => {
-    try {
-      const articlesRes = await fetch(
-        "https://dev.to/api/articles?per_page=10"
-      );
-      const topArticlesRes = await fetch(
-        "https://dev.to/api/articles?per_page=4&top=1"
-      );
-      const risingArticlesRes = await fetch(
-        "https://dev.to/api/articles?per_page=4&state=rising"
-      );
-
-      const articlesData = await articlesRes.json();
-      const topArticlesData = await topArticlesRes.json();
-      const risingArticlesData = await risingArticlesRes.json();
-
-      setArticles(articlesData);
-      setFilteredArray(articlesData);
-      setTopArticlesData(topArticlesData);
-      setRisinArticlesData(risingArticlesData);
-    } catch (error) {
-      console.log(error);
-    }
+    const res = await fetch("https://randomuser.me/api/?results=10");
+    const data = await res.json();
+    setUsers(data?.results);
+    setIsLoading(false);
   };
 
   useEffect(() => {
+    setIsLoading(true);
     fetchData();
   }, []);
 
-  const handleSearch = (event) => {
-    const filteredArticles = articles.filter((article) =>
-      article.title.toLowerCase().includes(event.target.value.toLowerCase())
+  if (isLoading) {
+    return (
+      <div className="flex gap-[10px] p-[20px]">
+        {Array(8)
+          .fill("1")
+          .map(() => {
+            return (
+              <div className="border border-blue-300 shadow rounded-md  w-[200px] h-[300px]">
+                <div className="animate-pulse flex ">
+                  <div className="flex-1 space-y-2 py-1">
+                    <div className="h-[20px] bg-slate-700 rounded"></div>
+                    <div className="space-y-3">
+                      <div className="h-[140px] bg-slate-700 rounded"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     );
-    setFilteredArray(filteredArticles);
-  };
+  }
 
   return (
-    <div>
-      <div className="flex w-[100%] justify-center">
-        <input
-          onChange={handleSearch}
-          type="text"
-          className="border-[2px] border-red-500 w-[200px] h-[50px]"
-        />
-      </div>
-      <h1 className="text-[50px]">TOP</h1>
-      <div className="flex gap-3 flex-wrap">
-        {topArticlesData.map((article) => {
+    <div className="flex gap-[10px] p-[20px]">
+      {users &&
+        users?.map((user, index) => {
           return (
-            <div className="border-[2px] rounded-md border-red-500 m-3 h-[200px] w-[300px] p-[10px]">
-              <img src={article.cover_image} alt="" />
-              {article.title}
+            <div
+              key={index}
+              className="border-[1px] border-black rounded w-[200px] h-[300px]"
+            >
+              <div>
+                {user?.name?.first} {user?.name?.last}
+              </div>
+              <img className="w-full mt-2" src={user?.picture?.medium} alt="" />
             </div>
           );
         })}
-      </div>
-
-      <h1 className="text-[50px]">Rising</h1>
-      <div className="flex gap-3 flex-wrap">
-        {risinArticlesData.map((article) => {
-          return (
-            <div className="border-[2px] rounded-md border-red-500 m-3 h-[200px] w-[300px] p-[10px]">
-              <img src={article.cover_image} alt="" />
-              {article.title}
-            </div>
-          );
-        })}
-      </div>
-
-      <h1 className="text-[50px]">ALL blogs</h1>
-      <div className="flex gap-3 flex-wrap">
-        {filteredArray.map((article) => {
-          return (
-            <div className="border-[2px] rounded-md border-red-500 m-3 h-[200px] w-[300px] p-[10px]">
-              <img src={article.cover_image} alt="" />
-              {article.title}
-            </div>
-          );
-        })}
-      </div>
     </div>
   );
 }

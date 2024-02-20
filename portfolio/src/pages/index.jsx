50d874c01c03e2343847d87587d9422ac1147e58
Dashka22 <dashka.nest1@gmail.com>
2024-02-20 17:06:21 +0800

    ssr

--- portfolio/src/pages/index.jsx
+++ portfolio/src/pages/index.jsx
@@ -1,16 +1,29 @@
-import { useState, useEffect } from "react";
+import Link from "next/link";
 
-export default function Home() {
-  const [users, setUsers] = useState([]);
+export default function Home({ articles }) {
+  return (
+    <div className="flex gap-[10px]">
+      {articles.map((article) => {
+        return (
+          <Link href={`/${article.slug}`}>
+            <div className="border-[1px] border-solid border-black rounded-md w-[200px]">
+              <img className=" w-[200px]" src={article.social_image} alt="" />
+              <h1>{article.title}</h1>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
 
-  const fetchData = async () => {
-    const res = await fetch("https://randomuser.me/api/?results=10");
-    setUsers(await res.json());
+export async function getServerSideProps() {
+  const res = await fetch("https://dev.to/api/articles?username=gereltuyamz");
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return <div>{users && users?.map((user) => <>{user?.name?.first}</>)}</div>;
 }
--- portfolio/src/pages/promise.jsx
+++ portfolio/src/pages/promise.jsx
@@ -1,44 +1,4 @@
-import { useEffect, useState } from "react";
-
 export default function Promise() {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchData = async () => {
-    const res = await fetch("https://randomuser.me/api/?results=10");
-    const data = await res.json();
-    setUsers(data?.results);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="flex gap-[10px] p-[20px]">
-        {Array(8)
-          .fill("1")
-          .map(() => {
-            return (
-              <div className="border border-blue-300 shadow rounded-md  w-[200px] h-[300px]">
-                <div className="animate-pulse flex ">
-                  <div className="flex-1 space-y-2 py-1">
-                    <div className="h-[20px] bg-slate-700 rounded"></div>
-                    <div className="space-y-3">
-                      <div className="h-[140px] bg-slate-700 rounded"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    );
-  }
-
   return (
     <div className="flex gap-[10px] p-[20px]">
       {users &&

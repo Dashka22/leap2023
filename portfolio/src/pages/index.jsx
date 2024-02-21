d5eae3464d403dff91662e9765d8886306b87056
Dashka22 <dashka.nest1@gmail.com>
2024-02-21 16:55:55 +0800

    SSG ISR uzev

--- portfolio/src/pages/index.jsx
+++ portfolio/src/pages/index.jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Home({ articles }) {
   return (
-    <div className="flex gap-[10px]">
+    <div className="flex gap-[10px] p-[20px]">
       {articles.map((article) => {
         return (
           <Link href={`/${article.slug}`}>
-            <div className="border-[1px] border-solid border-black rounded-md w-[200px]">
+            <div className="border-[1px] border-solid border-black rounded-md w-[200px] h-[200px]">
               <img className=" w-[200px]" src={article.social_image} alt="" />
               <h1>{article.title}</h1>
             </div>
@@ -17,10 +17,9 @@ export default function Home({ articles }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch("https://dev.to/api/articles?username=gereltuyamz");
   const articles = await res.json();
-
   return {
     props: {
       articles,

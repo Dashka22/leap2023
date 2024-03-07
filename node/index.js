7459b23d9d275bc82672d3deb2b5f1b8d1b6e53e
Dashka22 <dashka.nest1@gmail.com>
2024-03-07 17:04:21 +0800

    bcrypt

--- node/index.js
+++ node/index.js
@@ -1,19 +1,33 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const port = 8080;
 const app = express();
+app.use(express.json());
+
+let users = [];
 
 app.get("/", (req, res) => {
   res.send("Hello server deer huselt tani irlee");
 });
 
-//api gsn path ruu GET huselt ireheer json butsaanaa
-app.get("/leap/articles", (req, res) => {
-  res.json({ name: "dashka" });
+app.post("/signup", async (req, res) => {
+  const { name, password } = req.body;
+  const salt = bcrypt.genSaltSync(1);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  users.push({ name: name, password: hashedPassword });
+  res.send("User created succesfully");
 });
 
-app.post("/", (req, res) => {
-  res.send("Post huselt irsen");
+app.post("/signin", async (req, res) => {
+  const { name, password } = req.body;
+  const filteredUser = users.filter((user) => user.name === name);
+  const isValid = await bcrypt.compare(password, filteredUser[0].password);
+
+  if (isValid) {
+    res.send("login succesfully");
+  }
+  res.send("password & username not valid");
 });
 
 app.listen(port, () => {
